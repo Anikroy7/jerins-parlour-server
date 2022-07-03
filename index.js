9570afd9175fae78380afe300fac3ee571c5c1d0
Anikroy7 <roy645800@gmail.com>
2022-07-03 20:15:58 +0600

    review and loaded order by email api implement

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const express = require('express')
 const app = express()
 const port = process.env.PORT || 5000
@@ -17,7 +18,6 @@ app.get('/', (req, res) => {
 })
 
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.saft5.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
@@ -29,13 +29,53 @@ async function run() {
 
         await client.connect()
         const serviceCollection = client.db('jerins_parlour').collection('services');
+        const reviewCollection = client.db('jerins_parlour').collection('reviews');
+        const orderCollection = client.db('jerins_parlour').collection('orders');
+
+
         app.get('/services', async (req, res) => {
             const query = {};
             const result = await serviceCollection.find(query).toArray();
             res.send(result)
         })
 
+        app.get('/reviews', async (req, res) => {
+            const query = {};
+            const result = await reviewCollection.find(query).toArray()
+            res.send(result);
+        })
 
+        app.put('/order', async (req, res) => {
+            const order = req.body;
+            console.log(order);
+            const name = req.body.name;
+            const filter = { name: name }
+            const options = { upsert: true };
+            const updateDoc = {
+                $set: order
+            };
+            const result = await orderCollection.updateOne(filter, updateDoc, options)
+            res.send(result)
+        })
+        app.get('/order/:email', async (req, res) => {
+            const email = req.params.email;
+            console.log(email);
+            const query = { email: email };
+            const result = await orderCollection.find(query).toArray()
+            res.send(result)
+        })
+
+        app.get('/services/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const result = await serviceCollection.findOne(query);
+            res.send(result)
+        })
+        app.post('/review', async (req, res) => {
+            const review = req.body;
+            const result = await reviewCollection.insertOne(review);
+            res.send(result)
+        })
     }
     finally {
 
